f72c28c396c28b430fc1a125d917e143c45add54
JP Tyndall <jp.tyndall@luckie.com>
2019-05-20 16:45:49 -0400

    add firebase integration

--- content.js
+++ content.js
@@ -1,6 +1,45 @@
 $(function() {
+  // Build safari scene
   var safariScene = document.createElement('div');
   safariScene.classList.add('safari-scene');
-  safariScene.innerText = "I exist!";
+  var grass = document.createElement('div');
+  grass.classList.add('grass');
+  // grass.classList.add('is-animated');
+  safariScene.append(grass);
+  var img = document.createElement('img');
+  img.src = "https://ibin.co/4gzPObhRjgE0.png";
+  grass.append(img);
   $('body').append(safariScene);
+
+  // Build encounter scene
+  var encounterScene = document.createElement('div');
+  encounterScene.classList.add('encounter-scene');
+  var encounterStage = document.createElement('div');
+  encounterStage.classList.add('encounter-stage');
+  encounterScene.append(encounterStage);
+  var encounterTextbox = document.createElement('div');
+  encounterTextbox.classList.add('encounter-textbox');
+  encounterScene.append(encounterTextbox);
+  var encounterText = document.createElement('p');
+  encounterText.classList.add('encounter-text');
+  encounterText.innerHTML = "[Test] has appeared!";
+  encounterTextbox.append(encounterText);
+  var textArrow = document.createElement('div');
+  textArrow.classList.add('text-arrow');
+  encounterTextbox.append(textArrow);
+  $('body').append(encounterScene);
+
+  setTimeout(function() {
+    $(grass).click(function() {
+      showEncounter();
+    });
+  }, 1000);
 });
+
+function startEncounter() {
+  $('.grass').addClass('is-animated');
+}
+
+function showEncounter() {
+  $('.encounter-scene').toggle();
+}
--- popup.js
+++ popup.js
@@ -1,4 +1,45 @@
 $(function() {
+  console.log("Ready to authenticate!");
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAlT2nuMB1NuPC6p1pPSfZ3AgKcdCT2ZzM",
+    authDomain: "pokemon-web-safari.firebaseapp.com",
+    databaseURL: "https://pokemon-web-safari.firebaseio.com",
+    projectId: "pokemon-web-safari",
+    storageBucket: "pokemon-web-safari.appspot.com",
+    messagingSenderId: "179641627493",
+    appId: "1:179641627493:web:0dc991c6ee006b39"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  // Initialize firestore
+  var db = firebase.firestore();
+
+  // Initialize the FirebaseUI Widget using Firebase.
+  var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+  ui.start('#firebaseui-auth-container', {
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        // User successfully signed in.
+        // Return type determines whether we continue the redirect automatically
+        // or whether we leave that to developer to handle.
+        alert("welcome!");
+        return false;
+      },
+      uiShown: function() {
+        // The widget is rendered.
+        // Hide the loader.
+        document.getElementById('loader').style.display = 'none';
+      }
+    },
+    signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    ],
+  });
+
   console.log("Ready to safari!");
   $.getJSON("pokedex.json", function(result) {
     var list = $(".pokemon-list");
